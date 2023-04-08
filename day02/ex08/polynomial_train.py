72fb92222153a4347fc44e6fae5d33989056d156
maxime-42 <mkayumba@student.42.fr>
2023-04-08 18:49:16 +0200

    ex08 compris

--- day02/ex08/polynomial_train.py
+++ day02/ex08/polynomial_train.py
@@ -6,34 +6,38 @@ from day02.ex05.mylinearregression import MyLinearRegression as MyLR
 
 from day02.ex07.polynomial_model import add_polynomial_features
 
-x = np.arange(1,11).reshape(-1,1)
+from pydantic import BaseModel, conlist, Config
 
-y = np.array([[ 1.39270298],
-[ 3.88237651],
-[ 4.37726357],
-[ 4.63389049],
-[ 7.79814439],
-[ 6.41717461],
-[ 8.63429886],
-[ 8.19939795],
-[10.37567392],
-[10.68238222]])
-# plt.scatter(x,y)
-# plt.show()
+class LinearRegression(BaseModel):
+    """ structure """
+    theta: np.ndarray
+    max_iter: int
+    alpha: int
+    
+    class Config:
+        arbitrary_types_allowed = True
 
-# Build the model:
-x_ = add_polynomial_features(x, 3)
-my_lr = MyLR(np.ones(4).reshape(-1,1))
-my_lr.fit_(x_, y)
-print(x_.shape)
-print(y.shape)
+def score(theta:np.ndarray):
+    pass
 
-# Plot:
-## To get a smooth curve, we need a lot of data points
-continuous_x = np.arange(1,10.01, 0.01).reshape(-1,1)
-x_ = add_polynomial_features(continuous_x, 3)
-y_hat = my_lr.predict_(x)
-# print(x_)
-plt.scatter(x, y)
-plt.plot(continuous_x, y_hat, color='orange')
-plt.show()
+def main():
+    data = pd.read_csv("are_blue_pills_magics.csv")
+    x = data["Micrograms"]
+    y = data["Score"]
+    
+    # training_array:Trains = Trains[6]
+    # training_array= Trains(theta=np.array([[1], [1]]).reshape(-1,1), 
+    #                            max_iter=1e-2, alpha=1e2)
+    
+    # thetat2 = np.array([[1], [1], [1]]).reshape(-1,1),
+    # thetat3 = np.array([[1], [1], [1], [1]]).reshape(-1,1),
+    # theta4 = np.array([[-20],[ 160],[ -80],[ 10],[ -1]]).reshape(-1,1)
+    # theta5 = np.array([[1140],[ -1850],[ 1110],[ -305],[ 40],[ -2]]).reshape(-1,1)
+    # theta6 = np.array([[9110],[ -18015],[ 13400],[ -4935],[ 966],[ -96.4],[ 3.86]]).reshape(-1,1)
+    #  = [theta1, thetat2, thetat3, theta4, theta5, theta6]
+    # alpha = [1e-2, 1e-3, 1e-5, 1e-6, 1e-8,  1e-9]
+    # max_iter = [1e2, 1e3, 1e5, 1e6, 1e8,  1e9]
+    # for theta in [theta1, thetat2, thetat3, theta4, theta5, theta6]:
+    # print(training_array[0])  
+if __name__ == "__main__":
+    main()
